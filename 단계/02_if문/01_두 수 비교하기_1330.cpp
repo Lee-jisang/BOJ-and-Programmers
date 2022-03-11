e4d30c706fe417b41a51472e630b03a1a6ca06f5
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-03-11 12:55:34 +0900

    Update 01_두 수 비교하기_1330.cpp

--- 단계/02_if문/01_두 수 비교하기_1330.cpp
+++ 단계/02_if문/01_두 수 비교하기_1330.cpp
@@ -1,18 +1,29 @@
+/*
+문제
+두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A와 B가 주어진다. A와 B는 공백 한 칸으로 구분되어져 있다.
+
+출력
+첫째 줄에 다음 세 가지 중 하나를 출력한다.
+
+A가 B보다 큰 경우에는 '>'를 출력한다.
+A가 B보다 작은 경우에는 '<'를 출력한다.
+A와 B가 같은 경우에는 '=='를 출력한다.
+*/
+
+
 #include <iostream>
 using namespace std;
 
-int main() {
-    int a;
-    cin >> a;
-    if (90 <= a && a <= 100) //���� and ������ �ٿ�����
-        cout << "A";
-    else if (80 <= a && a <= 89)
-        cout << "B";
-    else if (70 <= a && a <= 79)
-        cout << "C";
-    else if (60 <= a && a <= 69)
-        cout << "D";
-    else
-        cout << "F";
-
+int main(){
+    int a,b;
+    cin >> a >> b;
+    if(a>b)
+        cout << ">";
+    if(a<b)
+        cout << "<";
+    if(a==b)
+        cout << "==";  
 }
