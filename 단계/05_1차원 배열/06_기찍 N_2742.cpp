90e4e3128216511433614f075f94d1dfdbe11492
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-03-21 21:59:43 +0900

    Update 06_기찍 N_2742.cpp

--- 단계/05_1차원 배열/06_기찍 N_2742.cpp
+++ 단계/05_1차원 배열/06_기찍 N_2742.cpp
@@ -1,22 +1,43 @@
 /*
 문제
-자연수 N이 주어졌을 때, N부터 1까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+"OOXXOXXOOO"와 같은 OX퀴즈의 결과가 있다. O는 문제를 맞은 것이고, X는 문제를 틀린 것이다. 문제를 맞은 경우 그 문제의 점수는 그 문제까지 연속된 O의 개수가 된다. 예를 들어, 10번 문제의 점수는 3이 된다.
+"OOXXOXXOOO"의 점수는 1+2+0+0+1+0+0+1+2+3 = 10점이다.
+OX퀴즈의 결과가 주어졌을 때, 점수를 구하는 프로그램을 작성하시오.
 
 입력
-첫째 줄에 100,000보다 작거나 같은 자연수 N이 주어진다.
+첫째 줄에 테스트 케이스의 개수가 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있고, 길이가 0보다 크고 80보다 작은 문자열이 주어진다. 문자열은 O와 X만으로 이루어져 있다.
 
 출력
-첫째 줄부터 N번째 줄 까지 차례대로 출력한다.
+각 테스트 케이스마다 점수를 출력한다.
 */
- 
+
 
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
-    int N;
-    cin >> N;
-    
-    for(int i=N; i>0; i--)
-        cout << i << '\n';   
+int main() {
+    int T; //테스트 케이스 수
+    cin >> T;
+
+    string str_arr[T];
+
+
+    for (int i = 0; i < T; i++) {
+        int cumulative = 0; //누적개수
+        int sum = 0;
+
+        cin >> str_arr[i];
+
+        for (int j = 0; j < str_arr[i].length(); j++) {
+            if (str_arr[i][j] == 'O') { //대문자로 해야됨
+                cumulative++;
+                sum += cumulative;
+            }
+            else
+                cumulative = 0;
+        }
+        cout << sum << "\n";
+    }
+
 }
