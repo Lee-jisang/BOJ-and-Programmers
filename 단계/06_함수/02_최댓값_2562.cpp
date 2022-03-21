5118ae5d4cff3bd01d284205c4c21303262b0fd2
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-03-22 02:26:19 +0900

    Update 02_최댓값_2562.cpp

--- 단계/06_함수/02_최댓값_2562.cpp
+++ 단계/06_함수/02_최댓값_2562.cpp
@@ -1,39 +1,38 @@
-/*
-문제
-9개의 서로 다른 자연수가 주어질 때, 이들 중 최댓값을 찾고 그 최댓값이 몇 번째 수인지를 구하는 프로그램을 작성하시오.
-예를 들어, 서로 다른 9개의 자연수
-3, 29, 38, 12, 57, 74, 40, 85, 61
-이 주어지면, 이들 중 최댓값은 85이고, 이 값은 8번째 수이다.
-
-입력
-첫째 줄부터 아홉 번째 줄까지 한 줄에 하나의 자연수가 주어진다. 주어지는 자연수는 100 보다 작다.
-
-출력
-첫째 줄에 최댓값을 출력하고, 둘째 줄에 최댓값이 몇 번째 수인지를 출력한다.
-*/
 
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
+int dex(int n) {
+    if (n < 100) {
+        n = n + n / 10 + n % 10;
+        return n;
+    }
+    else if (n < 1000) {
+        n = n + n/100 + n/10%10 + n % 10;
+        return n;
+    }
+    else if (n < 10000) {
+        n = n + n/1000 + n/100%10 + n/10%10 + n%10;
+        return n;
+    }
+}
+
+
 int main() {
-    int a[9]; //9개의 서로 다른 자연수
-    int max = 0;
-    int maxindex;
-    int cnt = 0;
+    int b[10000];
+    for (int i = 0; i < 10000; i++)
+        b[i] = i;
 
-    for (int i = 0; i < 9; i++) {
-        cin >> a[i];
+    int n=1;
+    while (1) {
+        if (n > 999) break;
+        cout << n<< ":" << dex(n) << "\n";
+        
+        n++;
+        
     }
-
-    for (int i = 0; i < 9; i++) {
-        // 이전값들 중 최댓값보다 i번째 값이 더 크다면 최댓값과 그 인덱스를 갱신
-        if (a[i] > max) {
-            max = a[i];
-            maxindex = i;
-        }
-    }
-
-    cout << max << endl;
-    cout << maxindex+1 << endl; //배열은 0부터 인덱스가 시작되므로 1플러스 해주는거 잊지말기
     
+
+
 }
