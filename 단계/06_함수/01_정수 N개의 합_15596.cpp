a40e8e1bf2de9bc4a3c76918258ee96ce35a739f
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-03-22 00:56:14 +0900

    Update and rename 01_최소, 최대_10818.cpp to 01_정수 N개의 합_15596.cpp

---
+++ 단계/06_함수/01_정수 N개의 합_15596.cpp
@@ -0,0 +1,25 @@
+/*
+문제
+정수 n개가 주어졌을 때, n개의 합을 구하는 함수를 작성하시오.
+
+작성해야 하는 함수는 다음과 같다.
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+long long sum(vector<int> &a) { //배열함수 호출법
+	long long ans = 0;
+    for(int i=0; i<a.size(); i++){ //vector는 length() 가 아니라 size()!
+        ans = ans +a[i];
+    }
+	return ans;
+}
+
+int main() {
+
+    vector<int> a = {1,2,3,4,5};
+    cout << sum(a);
+
+}
