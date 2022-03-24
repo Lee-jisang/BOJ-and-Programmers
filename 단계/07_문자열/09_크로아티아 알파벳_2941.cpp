5f22d932354ab58bbf2cdf4a656ab2df0346be99
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-03-24 15:41:37 +0900

    Update 09_크로아티아 알파벳_2941.cpp

--- 단계/07_문자열/09_크로아티아 알파벳_2941.cpp
+++ 단계/07_문자열/09_크로아티아 알파벳_2941.cpp
@@ -139,3 +139,54 @@ int main() {
     
 
 }
+
+
+//else 붙이면 맞았습니다..?
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+   
+    int cnt = 0;
+
+
+    int i = 0;
+    while (i < s.length()) {
+        if (s[i] == 'c') {
+            if (s[i + 1] == '=' || s[i + 1] == '-') {
+                i++;
+            }
+        }
+        else if (s[i] == 's' || s[i] == 'z') {
+            if (s[i + 1] == '=') {
+                i++;
+            }
+        }
+        else if (s[i] == 'l' || s[i] == 'n') {
+            if (s[i + 1] == 'j') {
+                i++;
+            }
+        }
+        else if (s[i] == 'd') {
+            if (s[i + 1] == '-') {
+                i++;
+            }
+            else if (s[i + 1] == 'z') {
+                if (s[i + 2] == '=')
+                    i = i + 2;
+            }
+        }
+        cnt++;
+        i++;
+    }
+    cout << cnt << "\n";
+        
+    
+
+    
+
+}
