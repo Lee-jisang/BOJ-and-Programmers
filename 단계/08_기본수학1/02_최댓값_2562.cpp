2697e68ea3a190254783e40086a8860c0d2dc41b
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-03-26 03:01:12 +0900

    Update 02_최댓값_2562.cpp

--- 단계/08_기본수학1/02_최댓값_2562.cpp
+++ 단계/08_기본수학1/02_최댓값_2562.cpp
@@ -1,34 +1,24 @@
-/*
-문제
-N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
-
-입력
-첫째 줄에 숫자의 개수 N (1 ≤ N ≤ 100)이 주어진다. 둘째 줄에 숫자 N개가 공백없이 주어진다.
-
-출력
-입력으로 주어진 숫자 N개의 합을 출력한다.
-*/
-
-
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
-    int N; //숫자의 개수
-    cin >> N; //1<=N<=100
+    int N;
+    cin >> N; //1<=N<=1000000000
 
-    string s;
-    cin >> s;
+    if (N == 1)
+        cout << 1;
 
-    // stoi(s) 문자열을 정수로 변환 
-    // s[i]-48, 문자단위(char)를 정수로 변환    
-    int sum = 0;
 
-    for (int i = 0; i < s.length(); i++) {
-        sum= sum + (s[i] - 48);//문자단위 정수변환
+    long double a = 2;
+    long double b = 7;
+    double sum=0.0;
 
+    for (long double i = 1; i <= 18257; i++) {
+        if (a <= N && N <= b)
+            cout << i + 1;
+        a = a + 6.0 * i;
+        b = b + 6.0 * (i+1);
     }
-    cout << sum;
+
 
 }
