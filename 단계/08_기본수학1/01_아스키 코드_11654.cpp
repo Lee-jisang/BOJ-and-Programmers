bdedc249de60c4fb5aedfa7cc3d88c134f56ecd7
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-03-25 15:20:01 +0900

    Update 01_아스키 코드_11654.cpp

--- 단계/08_기본수학1/01_아스키 코드_11654.cpp
+++ 단계/08_기본수학1/01_아스키 코드_11654.cpp
@@ -1,20 +1,27 @@
-/*
-문제
-알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
 
-입력
-알파벳 소문자, 대문자, 숫자 0-9 중 하나가 첫째 줄에 주어진다.
-
-출력
-입력으로 주어진 글자의 아스키 코드 값을 출력한다.
-*/
-
+//시간 초과 풀이
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
-    char a;
-    cin >> a;
-    
-    cout << int(a);
+int main() {
+
+	double A, B, C; //A:고정비용, B:가변비용, C:노트북 가격
+	cin >> A >> B >> C; //21억 이하의 자연수
+
+	int i = 0;
+
+	if (B >= C) {
+		cout << -1;
+		return 0;
+	}	
+	else {
+		while (1) { //무한루프를 쓰면 
+			if (A + (B * i) < C * i) {
+				cout << i;
+				return 0;
+			}
+			i++;
+		}
+	}
 }
