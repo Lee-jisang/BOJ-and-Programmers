27e4c6f2eba117c20b2eb2ad54f3285866a07af4
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-04-04 02:17:20 +0900

    Update 03_합_8393.cpp

--- 단계/09_기본수학 2/03_합_8393.cpp
+++ 단계/09_기본수학 2/03_합_8393.cpp
@@ -1,24 +1,49 @@
 /*
 문제
-n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+정수 N이 주어졌을 때, 소인수분해하는 프로그램을 작성하시오.
 
 입력
-첫째 줄에 n (1 ≤ n ≤ 10,000)이 주어진다.
+첫째 줄에 정수 N (1 ≤ N ≤ 10,000,000)이 주어진다.
 
 출력
-1부터 n까지 합을 출력한다.
+N의 소인수분해 결과를 한 줄에 하나씩 오름차순으로 출력한다. N이 1인 경우 아무것도 출력하지 않는다.
 */
 
 #include <iostream>
 using namespace std;
 
-int main(){
-    int n;
-    cin >> n;
+int main() {
+    int N;
+    cin >> N; //정수 N (1 ≤ N ≤ 10,000,000)
+
+    int cnt;
+
+    if (N == 1) return 0; //N이 1인 경우는 아무것도 출력하지 않는다.
+
+    for (int i = 2; i <= N; i++) {
+        while (N % i == 0) {
+            N = N / i;
+            cout << i << "\n";
+        }           
+    }
+
+/*
+    for (int i = 0; i < 990; i++) {
+        while (N % a[i] == 0) {
+            N = N / a[i];
+            cnt2[i]++;
+            cout << a[i] << "\n";
+        }
+        //cout << cnt2[i] << " ";
+    }
+*/
+    /*
+    while (N % 2 == 0) {
+        N=N / 2;
+        cnt++;
+    }
+
+    cout <<cnt;
+    */
     
-    int sum=0;
-    for(int i=1; i<=n; i++)
-        sum= sum+i;
-    
-    cout << sum;   
 }
