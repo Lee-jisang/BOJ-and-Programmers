8f9343903d1d1e89e34a27a020566219c307e896
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-04-09 02:57:33 +0900

    Update 06_골드바흐의 추측_9020.cpp

--- 단계/09_기본수학 2/06_골드바흐의 추측_9020.cpp
+++ 단계/09_기본수학 2/06_골드바흐의 추측_9020.cpp
@@ -10,7 +10,7 @@ int main() {
 
     for (int i = 2; i <= 10000; i++) {
         cnt = 0;
-        for (int j = 2; j <= i; j++) {
+        for (int j = 2; j <= i; j++) { 
             if (i % j == 0)
                 cnt++;
         }
