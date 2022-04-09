6d4fa4c3d6bb750e624140eed11cdf480fec43ea
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-04-10 01:15:58 +0900

    Update 체스판 다시 칠하기_1018.cpp

--- 분류/브루트포스 알고리즘/체스판 다시 칠하기_1018.cpp
+++ 분류/브루트포스 알고리즘/체스판 다시 칠하기_1018.cpp
@@ -1,3 +1,6 @@
+
+
+//비교만 하면되는데... , 경우의수 다출력!
 #include <iostream>
 #include <vector>
 #include <cmath>;
@@ -6,12 +9,11 @@ using namespace std;
 
 
 int main() {
+
 	
-	int N, M;
-	cin >> N >> M; //N과 M은 8보다 크거나 같고, 50보다 작거나 같은 자연수
-	
-	
-	char a[8][8] = { 
+
+
+	char a[8][8] = {
 		{'W','B','W','B','W','B','W','B'},
 		{'B','W','B','W','B','W','B','W'},
 		{'W','B','W','B','W','B','W','B'},
@@ -21,39 +23,59 @@ int main() {
 		{'W','B','W','B','W','B','W','B'},
 		{'B','W','B','W','B','W','B','W'}, };
 
-	char b[8][8] = {	
+	char b[8][8] = {
 		{'B','W','B','W','B','W','B','W'},
 		{'W','B','W','B','W','B','W','B'},
 		{'B','W','B','W','B','W','B','W'},
 		{'W','B','W','B','W','B','W','B'},
 		{'B','W','B','W','B','W','B','W'},
 		{'W','B','W','B','W','B','W','B'},
-		{'B','W','B','W','B','W','B','W'}, 
+		{'B','W','B','W','B','W','B','W'},
 		{'W','B','W','B','W','B','W','B'}, };
 
-	char c;
+	char c[51][51];
+
+	char d[8][8];
 
 
-	if (N == 8 && M == 8) {
-		int cnt1 = 0;
-		int cnt2 = 0;
-		for (int i = 0; i < N; i++) {		
-			for (int j = 0; j < M; j++) {
-				cin >> c;	
-				if (c != a[i][j]) cnt1++;
-				if (c != b[i][j]) cnt2++;					
-			}
-		}		
-		cout << min(cnt1, cnt2);
+	int N, M;
+	cin >> N >> M; //N과 M은 8보다 크거나 같고, 50보다 작거나 같은 자연수
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> c[i][j];
+		}
 	}
-	else {
+
+	int n2 = 0;
+	int m2 = 8;
+
+	for (int i = 0; i < N-8+1; i++) {//세로,3번
+		int n = 0;
+		int m = 8;	
 
 
+		for (int j = 0; j < M-8+1; j++) {//가로, 6번
+
+
+
+			for (int k = n2; k < m2; k++) {
+				for (int l = n; l < m; l++) {
+					
+					cout << c[k][l];
+					
+				}	
+				cout << "\n";	
+				
+			}			
+			n++;
+			m++;
+			cout << "\n";	
+		
+		}
+		n2++;
+		m2++;
 	}
-	
-
-
-
 
 
 }
