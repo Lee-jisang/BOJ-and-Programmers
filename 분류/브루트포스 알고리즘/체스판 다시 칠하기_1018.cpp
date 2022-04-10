0b40052395747d7094eaab0974be27ca8467f102
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-04-11 00:41:30 +0900

    Update 체스판 다시 칠하기_1018.cpp

--- 분류/브루트포스 알고리즘/체스판 다시 칠하기_1018.cpp
+++ 분류/브루트포스 알고리즘/체스판 다시 칠하기_1018.cpp
@@ -81,6 +81,100 @@ int main() {
 }
 
 
+//풀었다!! 비교해서 카운트 작은거 
+#include <iostream>
+#include <vector>
+#include <cmath>;
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+
+
+
+
+	char a[8][8] = {
+		{'W','B','W','B','W','B','W','B'},
+		{'B','W','B','W','B','W','B','W'},
+		{'W','B','W','B','W','B','W','B'},
+		{'B','W','B','W','B','W','B','W'},
+		{'W','B','W','B','W','B','W','B'},
+		{'B','W','B','W','B','W','B','W'},
+		{'W','B','W','B','W','B','W','B'},
+		{'B','W','B','W','B','W','B','W'}, };
+
+	char b[8][8] = {
+		{'B','W','B','W','B','W','B','W'},
+		{'W','B','W','B','W','B','W','B'},
+		{'B','W','B','W','B','W','B','W'},
+		{'W','B','W','B','W','B','W','B'},
+		{'B','W','B','W','B','W','B','W'},
+		{'W','B','W','B','W','B','W','B'},
+		{'B','W','B','W','B','W','B','W'},
+		{'W','B','W','B','W','B','W','B'}, };
+
+	char c[51][51];
+
+	char d[8][8];
+
+
+	int N, M;
+	cin >> N >> M; //N과 M은 8보다 크거나 같고, 50보다 작거나 같은 자연수
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> c[i][j];
+		}
+	}
+	int cnt1;
+	int cnt2;
+
+	vector<int> v;
+
+	int n2 = 0;
+	int m2 = 8;
+
+	for (int i = 0; i < N - 8 + 1; i++) {//세로,3번
+		int n = 0;
+		int m = 8;
+
+
+		for (int j = 0; j < M - 8 + 1; j++) {//가로, 6번
+			cnt1 = 0;
+			cnt2 = 0;
+
+
+			for (int k = n2; k < m2; k++) {
+				for (int l = n; l < m; l++) {
+					//cout << c[k][l];
+					if (c[k][l] != a[k - i][l - j]) cnt1++; //[k - i][l - j] 계속 0~8로 비교하기 위해서, 이 포인트
+					if (c[k][l] != b[k - i][l - j]) cnt2++;
+
+				}
+				//cout << "\n";
+
+			}
+			n++;
+			m++;
+			v.push_back(min(cnt1,cnt2));
+			//cout << cnt1 << " " << cnt2 << "\n";
+			//cout << "\n";
+
+		}
+		n2++;
+		m2++;
+	}
+
+	sort(v.begin(), v.end());
+
+	cout << v[0];
+
+
+}
+
+
+//함수형 풀이
 #include<iostream> 
 #include<algorithm> 
 using namespace std; 
