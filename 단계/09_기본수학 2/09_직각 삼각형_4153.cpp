8685288084a5beec48992773ab58e2fc8ea7b327
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-04-11 16:59:47 +0900

    Update 09_직각 삼각형_4153.cpp

--- 단계/09_기본수학 2/09_직각 삼각형_4153.cpp
+++ 단계/09_기본수학 2/09_직각 삼각형_4153.cpp
@@ -20,7 +20,7 @@ int main() {
         cin >> a >> b >> c;
         if (a == 0 && b == 0 && c == 0)
             return 0;
-
+        //피타고라스의 
         if ( (pow(a, 2) + pow(b, 2) == pow(c, 2)) 
           || (pow(a, 2) + pow(c, 2) == pow(b, 2))
           || (pow(b, 2) + pow(c, 2) == pow(a, 2)))
