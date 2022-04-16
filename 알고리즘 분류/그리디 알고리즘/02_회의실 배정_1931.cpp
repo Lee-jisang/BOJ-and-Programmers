2c72917296a9dcf9c589fc3e8db551ed1033ea47
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-04-16 18:13:00 +0900

    Update and rename 회의실 배정_1931.cpp to 02_회의실 배정_1931.cpp

--- 알고리즘 분류/그리디 알고리즘/02_회의실 배정_1931.cpp
+++ 알고리즘 분류/그리디 알고리즘/02_회의실 배정_1931.cpp
@@ -1,3 +1,4 @@
+//틀린 풀이,, 재귀나 함수를 이용하면 풀릴수도, 접근이 잘못된걸 수도 있음
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -40,19 +41,43 @@ int main() {
 					if (v[j].second <= v[k].first)
 						cout << v[k].first << " " << v[k].second << "\n";
 				
-
-
-				}
-				
+				}			
 			}
-
 		}
 		cout << "\n";	
 	}
+}
+
+//맞는 풀이
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;// N(1 ≤ N ≤ 100,000)
 
 
-	
+	vector<pair<int, int>> v;
+
+	int start, end;
+
+	for (int i = 0; i < N; i++) {	
+		cin >> start >> end;
+		v.push_back(pair<int, int>(end, start));//뒤집어서 넣고
 
 
+	}
 
+	sort(v.begin(), v.end());
+
+	int savetime = 0, ans = 0;
+	for (int i = 0; i < N; i++) {
+		if (savetime <= v[i].second) {//
+			savetime = v[i].first;
+			ans++;
+		}
+	}
+	cout << ans;
 }
