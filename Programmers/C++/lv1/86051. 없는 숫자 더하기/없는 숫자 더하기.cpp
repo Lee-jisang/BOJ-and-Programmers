c5017536e8cea293151eed549fce4b4fe676833c
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-07-29 16:13:45 +0900

    Update 없는 숫자 더하기.cpp

--- Programmers/C++/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
+++ Programmers/C++/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
@@ -20,3 +20,35 @@ int solution(vector<int> numbers) {
 
     return answer;
 }
+
+
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+
+    int answer = 45;
+
+    for (int i = 0 ; i < numbers.size() ; i++)
+        answer -= numbers[i];
+
+    return answer;
+}
