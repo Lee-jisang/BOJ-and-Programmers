71c84d4b9851ea3d8b786cf3b15443aa806794ee
Lee-jisang <dlwltkd2003@naver.com>
2022-07-29 16:13:07 +0900

    없는 숫자 더하기

---
+++ Programmers/C++/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> a = { 0,1,2,3,4,5,6,7,8,9 };
+
+int solution(vector<int> numbers) {
+    for (int i = 0; i < numbers.size(); i++) {
+        auto it = find(a.begin(), a.end(), numbers[i]);
+        if (it != a.end()) {
+            a.erase(a.begin() + (it - a.begin()));
+        }
+    }
+
+    int answer = 0;
+    for (int i = 0; i < a.size(); i++) {
+        answer += a[i];
+    }
+
+    return answer;
+}
