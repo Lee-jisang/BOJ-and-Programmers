21c5b67660c3736266ddfdfbf11b5ae27f1a825b
Lee-jisang <dlwltkd2003@naver.com>
2022-08-01 14:30:24 +0900

    음양 더하기

---
+++ Programmers/C++/lv1/76501. 음양 더하기/음양 더하기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+    for(int i=0; i<absolutes.size(); i++){
+        if(signs[i]==true){
+            answer += absolutes[i];
+        }else{
+            absolutes[i] = 0-absolutes[i];
+            answer += absolutes[i];
+        } 
+    }
+    return answer;
+}
